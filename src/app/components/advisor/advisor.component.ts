384d45e7217239ca7f13d5d026219c671542dfba
Spencer <selswick0010@outlook.com>
2021-01-13 04:46:25 -0500

    Added advisor logic and momentjs

--- src/app/components/advisor/advisor.component.ts
+++ src/app/components/advisor/advisor.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import * as moment from 'moment';
+import {
+  AnonymousSubject
+} from 'rxjs/internal/Subject';
 
 @Component({
   selector: 'app-advisor',
@@ -7,9 +14,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdvisorComponent implements OnInit {
 
-  constructor() { }
+  darkThroneTime: any;
+  nextTurnTime: any;
+  nextTurnTimeStamp: any;
 
-  ngOnInit(): void {
+  constructor() {
+    setInterval(() => {
+      this.getTime()
+    }, 1);
+    setInterval(() => {
+      this.getTurnTime()
+    }, 1);
+
   }
 
+  ngOnInit(): void {
+
+  }
+
+  getTime() {
+    this.darkThroneTime = moment();
+
+    return this.darkThroneTime;
+  }
+
+  getTurnTime() {
+    //get next turn timestamp
+    if (this.darkThroneTime.minutes() < 30) {
+      this.nextTurnTimeStamp = moment().set('minute', 30);
+    } else {
+      this.nextTurnTimeStamp = moment().set('minute', 60);
+    }
+
+    //convert to minutes left
+    // this.nextTurnTime = moment.duration(moment(this.nextTurnTimeStamp,"HH:mm:ss").diff(moment(this.darkThroneTime, "HH:mm:ss"),'seconds'));
+    
+    this.nextTurnTime = moment(this.nextTurnTimeStamp,"DD/MM/YYYY HH:mm:ss")
+        .diff(moment(this.darkThroneTime,"DD/MM/YYYY HH:mm:ss"));
+    console.log(moment(this.nextTurnTime).seconds);
+    
+    return this.nextTurnTime;
+  }
 }
